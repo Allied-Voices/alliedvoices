2f875eb1f7e7d1fb778c9e11453e2d856a891705
Pebanh <peter.banh95@gmail.com>
2020-08-21 15:04:15 -0400

    Remove unused variables

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import L from 'leaflet';
-import { Map, TileLayer, ZoomControl, Marker, Popup } from 'react-leaflet';
+import { Map, TileLayer, ZoomControl} from 'react-leaflet';
 import { Sidebar, Tab } from './Sidebar';
 
 class App extends Component {
