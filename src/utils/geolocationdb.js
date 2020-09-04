765a993cd24ca2758b8273c933165945320347d1
Pebanh <peter.banh95@gmail.com>
2020-09-04 01:50:14 -0400

    Add cb options to data fetching functions for flexibility in function use

--- src/utils/geolocationdb.js
+++ src/utils/geolocationdb.js
@@ -1,11 +1,18 @@
 const BASE_URL = 'https://geolocation-db.com/json/7733a990-ebd4-11ea-b9a6-2955706ddbf3'
 
-async function getLocation() {
+async function getLocation(cb) {
   const response = await fetch(BASE_URL, {
     method: "GET"
   })
   const responseData = await response.json();
-  return { lat: responseData.latitude, lng: responseData.longitude }
+
+  if (cb) {
+    cb()
+  } else {
+    return { lat: responseData.latitude, lng: responseData.longitude, generalLocations: [responseData.city, responseData.state] }
+  }
 }
 
 export { getLocation }
+
+// Current Return {"country_code":"CA","country_name":"Canada","city":"Toronto","postal":"M4Y","latitude":43.6656,"longitude":-79.383,"IPv4":"173.230.164.131","state":"Ontario"}
