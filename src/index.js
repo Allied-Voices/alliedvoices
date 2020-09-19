28ff6c8820c1ea8ed937067ce4bbfa53714d7c8e
Pebanh <peter.banh95@gmail.com>
2020-09-18 23:59:02 -0400

    Remove unused css and add browswer router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom'
 import App from './Pages/App/App';
-// import 'leaflet/dist/leaflet.css';
-
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
