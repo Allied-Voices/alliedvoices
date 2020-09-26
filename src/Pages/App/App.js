e8772196017eb306da0552851919118f728f3d05
Pebanh <peter.banh95@gmail.com>
2020-09-25 21:00:53 -0400

    Add new map page as part of the new redesign. New route is temporary to allow comparison between old and new map page

--- src/Pages/App/App.js
+++ src/Pages/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom'
 import LandingPage from '../LandingPage/LandingPage'
 import MapPage from '../MapPage/MapPage'
+import NewMapPage from '../NewMapPage/NewMapPage';
 import LocationContextProvider from '../../Context/LocationContext'
 import './App.css';
 
@@ -15,6 +16,9 @@ class App extends Component {
           </Route>
           <Route path="/landing">
             <LandingPage />
+          </Route>
+          <Route path="/new">
+            <NewMapPage />
           </Route>
         </Switch>
       </LocationContextProvider>
---
+++ src/Pages/NewMapPage/NewMapPage.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import NewMapPageStyles from './NewMapPageStyles.module.css'
+
+class NewMapPage extends Component {
+
+  render() {
+
+    return (
+      <div className={NewMapPageStyles.Container}>
+
+        <h1>Hello World</h1>
+
+      </div>
+    );
+  }
+}
+
+export default NewMapPage;
