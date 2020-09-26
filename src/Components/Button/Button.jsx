627a054aaec57a7f31001c0b4e2c442155d8bf92
Pebanh <peter.banh95@gmail.com>
2020-09-26 12:23:48 -0400

    Add new button component

---
+++ src/Components/Button/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ButtonStyles from './ButtonStyles.module.css'
+
+const Button = () => {
+  return (
+    <button className={ButtonStyles.Button}>
+
+    </button>
+  );
+}
+
+export default Button;
