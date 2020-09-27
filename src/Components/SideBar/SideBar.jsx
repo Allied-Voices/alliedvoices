48911a63a02f36b84c3bbebcddffbd079137e7ac
Pebanh <peter.banh95@gmail.com>
2020-09-27 15:11:46 -0400

    Add articles to side bar

--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Logo from '../Logo/Logo';
 import Search from '../Search/Search';
 import ButtonMenu from '../ButtonMenu/ButtonMenu'
 import Button from '../Button/Button'
+import { AppContext } from '../../Context/AppContext'
+import SideBarArticle from '../SideBarArticle/SideBarArticle'
 import SideBarStyles from './SideBar.module.css'
 
 const SideBar = () => {
+  const appContext = useContext(AppContext)
+
   return (
     <div className={SideBarStyles.Container}>
+
       <div className={SideBarStyles.Header}>
         <Logo primary />
         <h3>Allied Voices</h3>
       </div>
+
       <div className={SideBarStyles.SearchSection}>
         <Search />
       </div>
+
       <div className={SideBarStyles.FilterSection}>
         <Button label='Location' active />
         <ButtonMenu label='Type' />
@@ -22,6 +29,13 @@ const SideBar = () => {
         <ButtonMenu label='Time' />
         <ButtonMenu label='More Filters' />
       </div>
+
+      <div className={SideBarStyles.ArticleSection}>
+        {appContext.voices.rows && appContext.voices.rows.map((voice) =>
+          <SideBarArticle heading={voice.Name} date={voice.Date} lat={voice.lat} lng={voice.lng} publisher={voice.Publisher} type={voice.Type} />
+        )}
+      </div>
+
     </div>
   );
 }
