5d3f846239d76f9729d31571c18c28e345f44414
Pebanh <peter.banh95@gmail.com>
2020-09-27 12:11:23 -0400

    Change LocationContext to AppContext to include more data

---
+++ src/Context/AppContext.jsx
@@ -0,0 +1,49 @@
+import React, { Component, createContext } from 'react'
+import { getLocation } from '../utils/geolocationdb';
+import { getVoices, getResources } from "../utils/airtable"
+
+export const AppContext = createContext();
+
+class AppContextProvider extends Component {
+  state = {
+    lat: 39,
+    lng: -98,
+    locations: [],
+    voices: [],
+    resources: []
+  }
+
+  // Get Initial Location, Voices and Resources
+  componentDidMount = async () => {
+    const { lat, lng, locations } = await getLocation()
+    this.setState({
+      lat: 40.730610,
+      lng: -73.935242,
+      locations: ['New York'],
+    }, () => {
+
+      getVoices(this.state.lat, this.state.lng, (voices) => {
+        this.setState({
+          voices
+        })
+      })
+
+      getResources(this.state.locations, (resources) => {
+        this.setState({
+          resources
+        })
+      })
+
+    })
+  }
+
+  render() {
+    return (
+      <AppContext.Provider value={{ ...this.state }}>
+        {this.props.children}
+      </AppContext.Provider>
+    );
+  }
+}
+
+export default AppContextProvider;
