fe40cfe14acf3137f2fb14960b7d439490bd381e
Pebanh <peter.banh95@gmail.com>
2020-09-27 01:38:36 -0400

    Add Button Menu Option Component

---
+++ src/Components/ButtonMenuOption/ButtonMenuOption.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ButtonMenuOptionStyles from './ButtonMenuOptionStyles.module.css'
+
+const ButtonMenuOption = ({ option, onChange, selected, ...props }) => {
+  return (
+    <label key={option} className={ButtonMenuOptionStyles.CheckboxLabel} for={"MenuOption-" + option} >
+      <input
+        className={ButtonMenuOptionStyles.Checkbox}
+        id={"MenuOption-" + option}
+        type="checkbox"
+        onChange={onChange ? onChange : null}
+        checked={selected}
+      />
+      <span className={ButtonMenuOptionStyles.CheckboxSquare}>
+        <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" ariaHidden="true" role="presentation" focusable="false" style={{ margin: '3px auto 0 auto', display: 'block', fill: 'none', height: 16, width: 16, stroke: 'currentcolor', strokeWidth: 4, overflow: 'visible' }}><path fill="none" d="m4 16.5 8 8 16-16"></path></svg>
+      </span>
+      <p className={ButtonMenuOptionStyles.CheckboxText}>{option}</p>
+    </label>
+  );
+}
+
+export default ButtonMenuOption;
