2316ae44c0dc34c007f1e36c71d8949a698b8b14
Pebanh <peter.banh95@gmail.com>
2020-10-19 21:44:56 -0400

    Add alternative color scheme to button

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import ButtonStyles from './ButtonStyles.module.css'
+import ButtonStyles from './Button.module.css'
 
-const Button = ({ label, active, ...props }) => {
+const Button = ({ label, active, alternative, ...props }) => {
   var classNames = `${ButtonStyles.Button}`;
+
   if (active) {
     classNames += ` ${ButtonStyles.Active}`
+  }
+
+  if(alternative){
+    classNames += ` ${ButtonStyles.Alternative}`
   }
 
   return (
