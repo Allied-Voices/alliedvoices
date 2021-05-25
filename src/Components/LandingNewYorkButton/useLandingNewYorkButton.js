a66297af8f2a51db9726d7121547e110684f59b0
Pebanh <peter.banh95@gmail.com>
2021-05-25 00:04:49 -0400

    Remove landing search component, add new york button

---
+++ src/Components/LandingNewYorkButton/useLandingNewYorkButton.js
@@ -0,0 +1,13 @@
+import { useHistory } from 'react-router-dom';
+
+const useLandingNewYorkButton = () => {
+  let history = useHistory();
+
+  const goToMap = () => {
+    history.push('/map');
+  };
+
+  return { goToMap };
+};
+
+export default useLandingNewYorkButton;
