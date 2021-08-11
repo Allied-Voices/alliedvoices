c6e9a72ed6a4f908b4a567e734bcc285bb4ee932
Pebanh <peter.banh95@gmail.com>
2021-08-11 19:47:22 -0400

    [NOREF] Move App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom'
-import LandingPage from '../LandingPage/LandingPage'
-import MapPage from '../MapPage/MapPage';
-import AppContextProvider from '../../Context/AppContext'
-import '../../Style/Global.css';
+import LandingPage from './Pages/LandingPage/LandingPage'
+import MapPage from './Pages/MapPage/MapPage';
+import AppContextProvider from './Context/AppContext'
+import './Style/Global.css';
 
 class App extends Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom'
-import App from './Pages/App/App';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
