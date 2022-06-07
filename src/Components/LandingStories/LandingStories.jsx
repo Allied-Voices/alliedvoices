689f12634e0787aa679458b220c1958da5dfab1e
Latecoder-hub <cagatay.ersoy1@gmail.com>
2022-06-07 01:07:57 -0700

    placeholder fixed

--- src/Components/LandingStories/LandingStories.jsx
+++ src/Components/LandingStories/LandingStories.jsx
@@ -1,13 +1,17 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import LandingStoriesStyles from './LandingStories.module.css';
+import Placeholder from '../../Images/placeholder1.png';
 
+function addDefaultSrc(ev){
+    ev.target.src = Placeholder
+  }
 
 const LandingStories = React.memo(({ heading, image, url}) => {
     return (
 <div className={LandingStoriesStyles.StoryDiv}>
     <a href={url}>
     <div className={LandingStoriesStyles.StoryImgContainer}>
-    <img className={LandingStoriesStyles.StoryImg} src={image} alt="News Story"></img>
+    <img className={LandingStoriesStyles.StoryImg} src={image ?image:Placeholder} alt="News Story" onError={addDefaultSrc}></img>
     
     </div>
 
